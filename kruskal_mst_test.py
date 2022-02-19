dbef31a6d3ebd538fce3b94c969dfb39a7ae7a73
Dusan <dusan.trtica@hotmail.com>
2022-02-19 11:01:36 +0100

    Kruskal algorithm implememnted

--- kruskal_mst_test.py
+++ kruskal_mst_test.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 class Node:
     def __init__(self, rank, node_id, parent=None):
         self.rank = rank
@@ -37,14 +36,20 @@ class DisjointSetNaive:
         self.sets.add("".join(sorted(new_set)))
 
 
-def kruskal(graph, disjoint_set):
+def kruskal(edges, disjoint_set):
     # sort edges with regards of edge weight
-    # sorted(graph, lambda edge: edge[2])
-    return []
+    sorted_edges = sorted(edges, key=lambda edge: edge[2])
+
+    edges_in_mst = []
+    for (u, v, weight) in sorted_edges:
+        if disjoint_set.find(u) != disjoint_set.find(v):
+            edges_in_mst.append((u, v, weight))
+            disjoint_set.merge(u, v)
+
+    return sorted(edges_in_mst, key=lambda edge: edge[0])
 
 
-@pytest.mark.skip(reason="no way of currently testing this")
-def skip_test_spanning_tree():
+def test_spanning_tree():
     graph = [
         ('a', 'b', 2),
         ('a', 'c', 6),
@@ -59,14 +64,14 @@ def skip_test_spanning_tree():
         ('f', 'g', 5)
     ]
 
-    mst = kruskal(graph, DisjointSetNaive(graph))
+    mst = kruskal(graph, DisjointSetNaive(['a', 'b', 'c', 'd', 'e', 'f', 'g']))
     expected_mst = [
         ('a', 'b', 2),
         ('b', 'd', 3),
         ('b', 'e', 3),
         ('c', 'd', 1),
         ('c', 'f', 2),
-        ('f', 'g', 5)
+        ('d', 'g', 5)
     ]
 
     assert mst == expected_mst
@@ -81,12 +86,16 @@ def test_disjoint_set_naive():
 
     disjoint_set.merge('a', 'a')
     assert disjoint_set.sets == {'a', 'b', 'c', 'd', 'e'}
+    assert disjoint_set.find('b') == 'b'
 
     disjoint_set.merge('a', 'b')
     assert disjoint_set.sets == {'ab', 'c', 'd', 'e'}
+    assert disjoint_set.find('b') == 'a'
 
     disjoint_set.merge('b', 'c')
     assert disjoint_set.sets == {'abc', 'd', 'e'}
+    assert disjoint_set.find('c') == 'a'
+    assert disjoint_set.find('d') == 'd'
 
     disjoint_set.merge('a', 'c')
     assert disjoint_set.sets == {'abc', 'd', 'e'}
@@ -99,3 +108,4 @@ def test_disjoint_set_naive():
 
     disjoint_set.merge('a', 'd')
     assert disjoint_set.sets == {'abcde'}
+    assert disjoint_set.find('d') == 'a'
